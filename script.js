93c1c64c384e1f03740d96f19d6c7f617db5f84e
Amdrii <andreyk557@gmail.com>
2022-08-24 20:42:45 +0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+  VANTA.BIRDS({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundAlpha: 0.0,
+    color2: 0xff00f0,
+    wingSpan: 40.00,
+    separation: 100.00,
+    alignment: 77.00,
+    cohesion: 5.00,
+    quantity: 3.00
+  })
+
+  setTimeout(() => {
+    const main = document.querySelector('main')
+    // main.style.opacity = 1;
+    // main.style.filter = 'blur(0px)'
+  }, 1000)
+})
+
+
 
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
@@ -142,28 +169,4 @@ function clearElement(element) {
 render()
 
 
-window.addEventListener('DOMContentLoaded', () => {
-  VANTA.BIRDS({
-    el: "#vanta",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: true,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    backgroundAlpha: 0.0,
-    color2: 0xff00f0,
-    wingSpan: 40.00,
-    separation: 100.00,
-    alignment: 77.00,
-    cohesion: 5.00,
-    quantity: 3.00
-  })
 
-  setTimeout(() => {
-    const main = document.querySelector('main')
-    // main.style.opacity = 1;
-    // main.style.filter = 'blur(0px)'
-  }, 1000)
-})
